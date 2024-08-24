c3c96f2d6e7ce87e448c622d7ab2d97617135622
Ernani Raffo <ernani.raffo@icloud.com>
2024-08-23 22:51:45 -0700

    Pushing prototype

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraManager: MonoBehaviour {
+    public GameObject mainCamera;
+
+    void Start () {
+        mainCamera = Camera.main.gameObject;
+        mainCamera.transform.position = new Vector3(0, 0, -30);
+        mainCamera.GetComponent<Camera>().fieldOfView = 60f;
+    }
+
+    void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject player;
+    bool move = false;
+    bool moving = false;
+    float duration = 0.075f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject;
+        player.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement() {
+        float x = player.gameObject.transform.position.x;
+        float y = player.gameObject.transform.position.y;
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            y += 10;
+            move = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            x -= 10;
+            move = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            x += 10;
+            move = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+            y -= 10;
+            move = true;
+        }
+        // transform.position = new Vector3(x, y, 0);
+        if (move && !moving) {
+            move = false;
+            moving = true;
+            transform.DOMove(new Vector3(x, y, 0), duration).OnComplete(StopMoving);
+        }
+    }
+
+    private void StopMoving() {
+        Debug.Log("Stop moving!!");
+        moving = false;
+    }
+
+    public Vector3 GetPlayerPosition() {
+        return player.transform.position;
+    }
+}
