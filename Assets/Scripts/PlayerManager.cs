f3f73ca57febb25cf5a8fb85bdb4e6d35428dde9
reavisjordan <reavisjordan@gmail.com>
2024-08-25 14:57:17 -0700

    Created testing scene and altered player movement

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,49 +1,156 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using DG.Tweening;
+
+//public class PlayerManager : MonoBehaviour
+//{
+//    public GameObject player;
+//    bool move = false;
+//    bool movingX = false;
+//    bool movingY = false;
+//    float duration = 0.075f;
+//    public float speed = 20f;
+//    public float moveDistance = 10f; // added a move distance so we only need to change one variable instead of 4
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        player = gameObject;
+//        player.transform.position = new Vector3(0, 0, 0);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        MovePlayerForward();
+//        HandleMovement();
+//    }
+
+//    void HandleMovement()
+//    {
+//        float x = player.gameObject.transform.position.x;
+//        float y = player.gameObject.transform.position.y;
+//        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+//        {
+//            y += moveDistance;
+//            move = true;
+//        }
+//        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+//        {
+//            x -= moveDistance;
+//            move = true;
+//        }
+//        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+//        {
+//            x += moveDistance;
+//            move = true;
+//        }
+//        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+//        {
+//            y -= moveDistance;
+//            move = true;
+//        }
+//        // transform.position = new Vector3(x, y, 0);
+//        if (move && !movingX && !movingY)
+//        {
+//            move = false;
+//            movingX = true;
+//            movingY = true;
+//            transform.DOMoveX(x, duration).OnComplete(StopMovingX);
+//            transform.DOMoveY(y, duration).OnComplete(StopMovingY);
+//        }
+//    }
+
+//    private void StopMovingX()
+//    {
+//        movingX = false;
+//    }
+
+//    private void StopMovingY()
+//    {
+//        movingY = false;
+//    }
+
+//    public Vector3 GetPlayerPosition()
+//    {
+//        return player.transform.position;
+//    }
+
+//    public void MovePlayerForward()
+//    {
+//        player.transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour
+{
     public GameObject player;
     bool move = false;
     bool movingX = false;
     bool movingY = false;
     float duration = 0.075f;
     public float speed = 20f;
+    public float moveDistance;
+
+    // Movement grid size
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         player = gameObject;
         player.transform.position = new Vector3(0, 0, 0);
-    }
+
+        minX = -moveDistance;
+        maxX = moveDistance;
+        minY = -moveDistance;
+        maxY = moveDistance;
+}
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         MovePlayerForward();
         HandleMovement();
     }
 
-    void HandleMovement() {
+    void HandleMovement()
+    {
         float x = player.gameObject.transform.position.x;
         float y = player.gameObject.transform.position.y;
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-            y += 10;
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && y < maxY)
+        {
+            y += moveDistance;
             move = true;
         }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            x -= 10;
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && x > minX)
+        {
+            x -= moveDistance;
             move = true;
         }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-            x += 10;
+        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && x < maxX)
+        {
+            x += moveDistance;
             move = true;
         }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-            y -= 10;
+        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && y > minY)
+        {
+            y -= moveDistance;
             move = true;
         }
-        // transform.position = new Vector3(x, y, 0);
-        if (move && !movingX && !movingY) {
+
+        if (move && !movingX && !movingY)
+        {
             move = false;
             movingX = true;
             movingY = true;
@@ -52,19 +159,23 @@ public class PlayerManager : MonoBehaviour {
         }
     }
 
-    private void StopMovingX() {
+    private void StopMovingX()
+    {
         movingX = false;
     }
 
-    private void StopMovingY() {
+    private void StopMovingY()
+    {
         movingY = false;
     }
 
-    public Vector3 GetPlayerPosition() {
+    public Vector3 GetPlayerPosition()
+    {
         return player.transform.position;
     }
 
-    public void MovePlayerForward() {
+    public void MovePlayerForward()
+    {
         player.transform.position += new Vector3(0, 0, speed * Time.deltaTime);
     }
 }
