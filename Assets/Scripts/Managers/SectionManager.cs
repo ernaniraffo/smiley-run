96b04c965e730c89a8c0c281d080419c89b136e2
Ernani Raffo <ernani.raffo@icloud.com>
2024-09-10 21:22:26 -0700

    Created SectionManager, activating pillars instead of deactivating them

--- Assets/Scripts/Managers/SectionManager.cs
+++ Assets/Scripts/Managers/SectionManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SectionManager : MonoBehaviour {
     public GameObject sectionPrefab;
+    private int level = 1;
 
     // Start is called before the first frame update
     void Start() {
@@ -15,5 +16,24 @@ public class SectionManager : MonoBehaviour {
 
     public void SpawnNextSection(Vector3 pos, Quaternion rot) {
         Instantiate(sectionPrefab, pos, rot);
+    }
+
+    public void ActivatePillars(List<GameObject> pillars) {
+        // default values for number of pillars to enable
+        int minPillars = 1;
+        int maxPillars = 1;
+        if (level == 1) {
+            // when in level 1, max pillars enabled is 3
+            maxPillars = 3;
+        }
+        // figure out how many pillars to enable using random
+        int pillarsToEnable = Random.Range(minPillars, maxPillars + 1);
+        for (int i = 0; i < pillarsToEnable; i++) {
+            // get pillar to enable
+            int pillarIndex = Random.Range(0, pillars.Count);
+            pillars[pillarIndex].SetActive(true);
+            // remove from list so we dont roll it again in our RNG
+            pillars.RemoveAt(pillarIndex);
+        }
     }
 }
--- Assets/Scripts/Objects/Section.cs
+++ Assets/Scripts/Objects/Section.cs
@@ -10,20 +10,12 @@ public class Section : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         pillars = GetPillars();
-        DeactiveSomePillars();
+        Debug.Log(GameSingleton.instance.sectionManager);
+        GameSingleton.instance.sectionManager.ActivatePillars(pillars);
     }
 
     // Update is called once per frame
     void Update() {
-    }
-
-    void SpawnObject(Transform parent) {
-        // Vector3 spawnPosition = new Vector3(parent.position.x + (parent.lossyScale.x / 2) - 10,
-        //                                     parent.position.y + parent.lossyScale.y +
-        //                                         objectToSpawn.transform.lossyScale.y + 10,
-        //                                     parent.position.z);
-        // GameObject spawnedObject = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
-        // spawnedObject.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     List<GameObject> GetPillars() {
@@ -35,22 +27,5 @@ public class Section : MonoBehaviour {
             }
         }
         return pillars;
-    }
-
-    void DeactiveSomePillars() {
-        int section = 1;
-        int numToDeactive = pillars.Count;
-        if (section == 1) {
-            // get the number of pillars to deactive in the section
-            numToDeactive = (pillars.Count / 2) - Random.Range(1, pillars.Count / 2);
-        }
-        for (int i = 0; i < numToDeactive; i++) {
-            int index = Random.Range(0, pillars.Count);
-            // destroy the game object at the index
-            Destroy(pillars[i]);
-            // remove object at index so the next random number cannot be the same game object
-            // in case the generayed index by random is the same as previous
-            pillars.RemoveAt(index);
-        }
     }
 }
