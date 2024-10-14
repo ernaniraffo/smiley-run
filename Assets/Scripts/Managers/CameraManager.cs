400ef5f4876ece0f924eff5f8b81738ab8aab34c
Ernani Raffo <ernani.raffo@icloud.com>
2024-10-13 21:01:31 -0700

    Camera moves with player

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
-    public GameObject mainCamera;
-    private float distanceFromPlayer = 10;
+    public GameObject mainCamera {get; private set;}
+    public float distanceFromPlayer = 10;
+
+    public float rotationX = 5;
+    public float posY = 3;
 
     void Start() {
         mainCamera = Camera.main.gameObject;
 
-        mainCamera.transform.position = GameSingleton.instance.playerManager.GetPlayerPosition() -
-                                        new Vector3(0, 0, distanceFromPlayer);
+        mainCamera.transform.position = GameSingleton.instance.playerManager.GetPlayerPosition() +
+                                        new Vector3(0, posY, -distanceFromPlayer);
+        mainCamera.transform.rotation = Quaternion.Euler(rotationX, 0, 0);
         mainCamera.GetComponent<Camera>().fieldOfView = 60f;
     }
 
@@ -21,6 +25,10 @@ public class CameraManager : MonoBehaviour {
         if (GameSingleton.instance.sectionManager.stopSections) {
             mainCamera.transform.position = new Vector3(playerPosition.x, playerPosition.y,
                                                         playerPosition.z - distanceFromPlayer);
+        } else {
+            mainCamera.transform.position = GameSingleton.instance.playerManager.GetPlayerPosition() +
+                                        new Vector3(0, posY, -distanceFromPlayer);
+            mainCamera.transform.rotation = Quaternion.Euler(rotationX, 0, 0);
         }
     }
 }
